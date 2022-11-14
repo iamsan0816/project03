c933fd405a9e410ffaaf54256888d0eb8f4ebd4a
iamsan0816 <iamsan0816@gmail.com>
2022-11-14 10:29:42 +0800

    modify

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # deactivate
 # activate
 # git init
-# commit to repo     git satus    git add main.py
+# commit to repo     git satus    
+# git add main.py--暫存區      
+# git commit -m "add main.py"---進入commit
